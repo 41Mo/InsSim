81e13643431a39dc90f001d75c9fadda96d62ed3
Arck <sanya3@e.mail>
2021-12-05 21:57:57 +0300

    Проверка ввода

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import gui_template.template as tmpl
 import nav_alg as na
 import wx
+import re
 
 class MainWindow(tmpl.MainFrame): 
 
@@ -8,10 +9,44 @@ class MainWindow(tmpl.MainFrame):
     def __init__(self,parent): 
         tmpl.MainFrame.__init__(self,parent)  
         self.nav_alg = na.nav_alg()
+        self.is_input_invalid = False
+
+    def input_check_float(self, obj):
+        
+        result = re.search(r'[^0-9\.]',obj.GetValue(),flags=re.IGNORECASE)
+        obj.SetBackgroundColour("white")
+
+        if result != None:
+            obj.SetBackgroundColour("red")
+            self.is_input_invalid = True
+    
+    def input_check_int(self, obj):
+        
+        result = re.search(r'[^0-9]',obj.GetValue(),flags=re.IGNORECASE)
+        obj.SetBackgroundColour("white")
+
+        if result != None:
+            obj.SetBackgroundColour("red")
+            self.is_input_invalid = True
 
     def run_analysis(self, event):
-        selection = self.m_analysis_type.GetSelection()
+        
+        self.input_check_int(self.m_enter_freq)
+        self.input_check_int(self.m_enter_points_num)
+        self.input_check_float(self.m_enter_lat)
+        self.input_check_float(self.m_enter_lon)
+        self.input_check_float(self.m_enter_gyr_offset)
+        self.input_check_float(self.m_enter_acc_offset)
+        self.input_check_float(self.m_enter_gyr_drift)
+        self.input_check_float(self.m_enter_acc_drift)
 
+        if self.is_input_invalid:
+            wx.MessageBox("Введен некорректный символ")
+            self.is_input_invalid = False
+            return
+        
+
+        selection = self.m_analysis_type.GetSelection()
         self.nav_alg = na.nav_alg(
             frequency=int(self.m_enter_freq.GetValue()),
             points_count=int(self.m_enter_points_num.GetValue())
@@ -32,6 +67,7 @@ class MainWindow(tmpl.MainFrame):
             a_drift = float(self.m_enter_acc_drift.GetValue())
             self.nav_alg.set_a_body(a_drift, a_drift, a_drift)
             self.nav_alg.set_w_body(g_drift, g_drift, g_drift)
+
         wx.MessageBox("Running")
         self.nav_alg.analysis()
         self.nav_alg.plots()
