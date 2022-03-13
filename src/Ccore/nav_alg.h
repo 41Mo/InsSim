d180acf9f64b249bfe1f23b588b9e2ff5daa0cf0
Alex Molchanov <amol4anov@mail.ru>
2022-03-13 22:54:39 +0300

    prepare for c++ integration

--- src/Ccore/nav_alg.h
+++ src/Ccore/nav_alg.h
@@ -1,9 +1,10 @@
 #include "vectors.h"
+#include <stdint.h>
 
 class Nav {
 public:
-    //constructor
-    Nav();
+    // constructor
+    Nav(float phi, float lambda, int frequency);
     // destructor
     ~Nav() {};
 
@@ -13,31 +14,31 @@ public:
 	void speed();
 	void coordinates();
 	void ang_velocity_body_enu();
-	void aligment(float acc_x[], float acc_y[], float acc_z[], float heading, int size);
+	void aligment(float roll, float pitch, float yaw);
 	void iter(vec_body acc, vec_body gyr);
 private:
-	//Earth Parameters
-	const float R = 6378245.0;
-	const float U = ((3.14 * 15) / 180) / 3600; // ((Pi * Wspd_earth)) / 180 degree
-	const float G = 9.81;
+	// Earth constants
+	const float R{6378245.0};
+	const float U{((3.14 * 15) / 180) / 3600}; // ((Pi * Wspd_earth)) / 180 degree
+	const float G{9.81};
 
-	//Input values
-	float frequency = 10;
-	float dt = 1/frequency;
+	// variables
+	float c11{0}, c12{0}, c13{0}, c21{0}, c22{0}, c23{0}, c31{0}, c32{0}, c33{0}; //начальные элементы матрицы
 
-	//Default values
-	float c11 = 0, c12 = 0, c13 = 0, c21 = 0, c22 = 0, c23 = 0, c31 = 0, c32 = 0, c33 = 0; //начальные элементы матрицы
-	float phi = 0;
-	float lambda = 0;
-	float H = 0;
-	vec_body w_body = {0, 0, 0};
-	vec_body a_body = {0, 0, 0};
-	vec_enu v_enu = {0, 0, 0};
+	float H{0};
+	float teta{0};
+	float gamma{0};
+	float psi{0};
 
-	//Local variables
+	vec_body w_body {0, 0, 0};
+	vec_body a_body {0, 0, 0};
+	vec_enu v_enu {0, 0, 0};
+
+	// uninitialized variables
+	float phi;
+	float lambda;
+	int frequency;
+	float dt;
 	vec_enu w_enu;
 	vec_enu a_enu;
-	float teta = 0;
-	float gamma = 0;
-	float psi = 0;
 };
