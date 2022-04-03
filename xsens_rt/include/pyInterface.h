9d894aafcd2a54b530035076409a5d8105d59721
Alex Molchanov <amol4anov@mail.ru>
2022-04-03 18:30:56 +0300

    split constructor into init

--- xsens_rt/include/pyInterface.h
+++ xsens_rt/include/pyInterface.h
@@ -8,49 +8,50 @@
 
 class NavOut
 {
-  public:
-    float roll()  { return data[0]; };
-    float pitch() { return data[1]; };
-    float yaw()   { return data[2]; };
-    float lat()   { return data[3]; };
-    float lon()   { return data[4]; };
-    float v_e()   { return data[5]; };
-    float v_n()   { return data[6]; };
+public:
+  float roll() { return data[0]; };
+  float pitch() { return data[1]; };
+  float yaw() { return data[2]; };
+  float lat() { return data[3]; };
+  float lon() { return data[4]; };
+  float v_e() { return data[5]; };
+  float v_n() { return data[6]; };
 
-    void roll(float r)  {  data[0] = r; };
-    void pitch(float p) {  data[1] = p; };
-    void yaw(float y)   {  data[2] = y; };
-    void lat(float l)   {  data[3] = l; };
-    void lon(float l)   {  data[4] = l; };
-    void v_e(float v)   {  data[5] = v; };
-    void v_n(float v)   {  data[6] = v; };
-    void ax(float a)    {  data[7] = a; };
-    void ay(float a)    {  data[8] = a; };
-    void az(float a)    {  data[9] = a; };
-    void gx(float g)    {  data[10]= g; };
-    void gy(float g)    {  data[11]= g; };
-    void gz(float g)    {  data[12]= g; };
-    float data[13]{0};
+  void roll(float r) { data[0] = r; };
+  void pitch(float p) { data[1] = p; };
+  void yaw(float y) { data[2] = y; };
+  void lat(float l) { data[3] = l; };
+  void lon(float l) { data[4] = l; };
+  void v_e(float v) { data[5] = v; };
+  void v_n(float v) { data[6] = v; };
+  void ax(float a) { data[7] = a; };
+  void ay(float a) { data[8] = a; };
+  void az(float a) { data[9] = a; };
+  void gx(float g) { data[10] = g; };
+  void gy(float g) { data[11] = g; };
+  void gz(float g) { data[12] = g; };
+  float data[13]{0};
 };
 
-  class pyInterface
-  {
-  private:
-    /* data */
-    //NavOut data{};
-    Nav nav{};
-    Xsens xsens{};
-    SensData sens{};
-    void alignment(int64_t time);
-	  bool error = false;
+class pyInterface
+{
+private:
+  Nav nav{};
+  Xsens xsens{};
+  SensData sens{};
 
-  public:
-    pyInterface(float lat, float lon, uint16_t frq = 100, int64_t alignment_time = 60);
-    ~pyInterface();
-    bool ready() { return !error; }
-    //bool is_data_avail();
-    void get_data(NavOut *o);
-  };
+  bool error = false;
+  uint64_t alignment_t{};
+
+public:
+  pyInterface();
+  ~pyInterface();
+  bool ready() { return !error; }
+  void init(float lat, float lon, uint16_t frq);
+  void get_data(NavOut *o);
+  int scan_dev();
+  void alignment(int64_t time);
+};
 
 void print_sens_data(NavOut o);
 #endif
--- xsens_rt/src/pyInterface.cpp
+++ xsens_rt/src/pyInterface.cpp
@@ -3,31 +3,42 @@
 #include <iostream>
 #include <iomanip>
 
+pyInterface::pyInterface()
+{
+}
 
+pyInterface::~pyInterface()
+{
+}
 
-pyInterface::pyInterface(float lat, float lon, uint16_t frq, int64_t alignmentTime){
+void pyInterface::init(float lat, float lon, uint16_t frq) {
     xsens.set_frequency(frq);
-    if (xsens.init() !=0) error = true;
+    if (xsens.init() != 0)
+        error = true;
     nav.init(deg2rad(lat), deg2rad(lon), frq);
-    if (ready()) alignment(alignmentTime);
 }
 
-pyInterface::~pyInterface(){
+int pyInterface::scan_dev() {
+    return xsens.scanDevice();
 }
 
-void pyInterface::alignment(int64_t time) {
+void pyInterface::alignment(int64_t time)
+{
     int measCount = 0;
     SensData s{};
     SensData mean{};
 
     std::cout << "stating alignment for amount of " << time << " sec\n";
     time *= 1000;
-	int64_t startTime = XsTime::timeStampNow();
-	std::cout << std::setw(5) << std::fixed << std::setprecision(2);
+    int64_t startTime = XsTime::timeStampNow();
+    std::cout << std::setw(5) << std::fixed << std::setprecision(2);
     auto now = XsTime::timeStampNow() - startTime;
-	while (now <= time) {
-	    if (xsens.data_availible()) {
-	    	if (xsens.recieve(&s)) {
+    while (now <= time)
+    {
+        if (xsens.data_availible())
+        {
+            if (xsens.recieve(&s))
+            {
                 mean.acc.X += s.acc.X;
                 mean.acc.Y += s.acc.Y;
                 mean.acc.Z += s.acc.Z;
@@ -36,12 +47,12 @@ void pyInterface::alignment(int64_t time) {
                 mean.mag.Y += s.mag.Y;
                 mean.mag.Z += s.mag.Z;
                 measCount++;
-		        std::cout << "\r" << now << std::flush;
-           }
-       }
-       now = XsTime::timeStampNow() - startTime;
+                std::cout << "\r" << now << std::flush;
+            }
+        }
+        now = XsTime::timeStampNow() - startTime;
     }
-    std::cout<<std::endl;
+    std::cout << std::endl;
 
     mean.acc.X /= measCount;
     mean.acc.Y /= measCount;
@@ -53,9 +64,12 @@ void pyInterface::alignment(int64_t time) {
     nav.alignment(mean.acc.X, mean.acc.Y, mean.acc.Z, yaw);
 }
 
-void pyInterface::get_data(NavOut *data) {
-	if (xsens.data_availible()) {
-		if (xsens.recieve(&sens)) {
+void pyInterface::get_data(NavOut *data)
+{
+    if (xsens.data_availible())
+    {
+        if (xsens.recieve(&sens))
+        {
             nav.iter(sens.acc, sens.gyr);
 
             data->roll(rad2deg(nav.gamma));
@@ -77,16 +91,16 @@ void pyInterface::get_data(NavOut *data) {
 
 void print_sens_data(NavOut o)
 {
-	std::cout << std::setw(5) << std::fixed << std::setprecision(2);
-	std::cout << "\r"
-			  << "Lat:" << o.lat()
-			  << ", Lon:" << o.lon();
-	std::cout
-		<< " |Roll:" << o.roll()
-		<< ", Pitch:" << o.pitch()
-		<< ", Yaw:" << o.yaw();
-	std::cout
-		<< " |V_e:" << o.v_e()
-		<< ", V_n:" << o.v_n();
-	std::cout << std::flush;
+    std::cout << std::setw(5) << std::fixed << std::setprecision(2);
+    std::cout << "\r"
+              << "Lat:" << o.lat()
+              << ", Lon:" << o.lon();
+    std::cout
+        << " |Roll:" << o.roll()
+        << ", Pitch:" << o.pitch()
+        << ", Yaw:" << o.yaw();
+    std::cout
+        << " |V_e:" << o.v_e()
+        << ", V_n:" << o.v_n();
+    std::cout << std::flush;
 }
