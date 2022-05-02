bec000927a121f6ae5685412e4a6e9e9218a4050
Alex Molchanov <amol4anov@mail.ru>
2022-05-02 23:08:15 +0300

    fix temp corr

--- calib/temperature.py
+++ calib/temperature.py
@@ -36,28 +36,29 @@ frq= 100
 time = points/frq/60
 ini_temp = temp[0]
 use_lowpass = True
+#%%
 
+for i in range(3):
+    acc[:,i] = acc[:,i]-np.mean(acc[:,i])
 #%%
 if use_lowpass:
     acc_tmp = []
     for i in range(0,3):
         acc_tmp.append(filter(acc[:,i],10))
-    acc_corr = []
-    for i in range(0,len(acc_tmp[0])):
-        tmp = []
-        for j in range(0, 3):
-            tmp.append(acc_tmp[j][i])
-        acc_corr.append(tmp)
-    acc = np.array(acc_corr)
+    acc_tmp = np.array(acc_tmp)
+    acc=np.block([
+        (acc_tmp[0])[:,np.newaxis],
+        (acc_tmp[1])[:,np.newaxis],
+        (acc_tmp[2])[:,np.newaxis],
+    ])
 
 #%%
-
 a_k = []
 a_poly = []
 a_corr = []
-for i in range(0,3):
+for i in range(3):
     a_k_i, a_poly_i = temp_lstsq(ini_temp, temp, acc[:,i])
-    a_k.append(a_k)
+    a_k.append(a_k_i)
     a_poly.append(a_poly_i)
     a_corr_i = temp_corr(ini_temp,temp,a_k_i, acc[:,i])
     a_corr.append(
@@ -88,7 +89,7 @@ ax = [
 plots = [
     ["ax", "ax_poly"],
     ["ay", "ay_poly"],
-    ["ay", "ay_poly"]
+    ["az", "az_poly"]
 ]
 for i in range(3):
     df2.plot(
@@ -110,7 +111,7 @@ ax = [
 plots = [
     ["ax", "ax_corr"],
     ["ay", "ay_corr"],
-    ["ay", "ay_corr"]
+    ["az", "az_corr"]
 ]
 for i in range(3):
     df2.plot(
